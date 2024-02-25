f9559b093f3c023605a9e7ef4e387eb073ad61ae
willanderson <willanderson@users.noreply.github.com>
2024-02-25 19:48:46 +0000

    deploy: c71e780ad8b00b619d92ebcbde4c57b3d166b07b

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,4 +5,24 @@ $(window).scroll(function() {
     } else {
       $("#lastName").css('top', '22px'); /* Return to original position */
     }
+});
+  
+document.addEventListener('DOMContentLoaded', (event) => {
+  const videos = document.querySelectorAll('.lazy-video');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.play();
+        console.log('Video is playing:', entry.target.currentSrc); // Log when video starts playing
+      } else {
+        entry.target.pause();
+        console.log('Video is paused:', entry.target.currentSrc); // Log when video is paused
+      }
+    });
+  }, { threshold: 0.25 }); // Adjust threshold based on requirement
+
+  videos.forEach(video => {
+    observer.observe(video);
   });
+});
